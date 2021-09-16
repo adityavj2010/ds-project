27355b7372764089b415c22ad5697a9649752d5b
Aditya Jagtap <adityavj2010@gmail.com>
2021-09-16 16:11:10 -0400

    Further changes

--- backend/router.js
+++ backend/router.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { readData, writeData } = require("./Database");
+const { readData, writeData } = require("./database.js");
 const router = express.Router();
 
 router.get("/", function (req, res) {
--- server.js
+++ server.js
@@ -1,19 +1,17 @@
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
-const router = require('./backend/router');
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const router = require("./backend/router");
 const app = express();
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-console.log('router')
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.use('/api', router)
+app.use("/api", router);
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname+'/build'));
+app.use(express.static(__dirname + "/build"));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/build/index.html'));
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname + "/build/index.html"));
 });
 
 // Start the app by listening on the default Heroku port
